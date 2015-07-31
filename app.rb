94c950b4a616797f74538261be318c53d1205d3d
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-07-31 07:49:59 -0400

    Add root route

--- app.rb
+++ app.rb
@@ -8,3 +8,7 @@ get '/:number' do
   { number: number, factors: number.factors, odd: number.odd?,
     even: number.even?, prime: number.prime? }.to_json
 end
+
+get '/' do
+  'Welcome to the Number API, request informaiton about any number as a resource!'
+end
